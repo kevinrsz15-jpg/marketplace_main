7d24ed1e340ddc4796eb3455d50fe333c6c77a01
kevinrsz15 <kevinrsz15@gmail.com>
2026-04-28 17:24:27 -0700

    agregando

--- marketplace_main/settings.py
+++ marketplace_main/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'store',
 ]
 
 MIDDLEWARE = [
@@ -105,6 +106,8 @@ AUTH_PASSWORD_VALIDATORS = [
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
+
+AUTH_USER_MODEL = 'store.User'
 
 USE_I18N = True
 
