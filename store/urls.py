ea949edd4433da40fcce48d96ccc255dc67a0484
kevinrsz15 <kevinrsz15@gmail.com>
2026-05-11 16:03:52 -0700

    si

--- store/urls.py
+++ store/urls.py
@@ -6,4 +6,10 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+    # Cart
+path('cart/', views.cart_detail, name='cart_detail'),
+path('cart/add/<uuid:product_id>/', views.add_to_cart, name='add_to_cart'),
+path('cart/remove/<uuid:item_id>/', views.remove_from_cart, name='remove_from_cart'),
+path('cart/update/<uuid:item_id>/', views.update_cart_item, name='update_cart_item'),
 ]
+
